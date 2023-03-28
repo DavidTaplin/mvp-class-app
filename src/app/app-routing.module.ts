d3c4665e0ad840323164c23a543e02cadb77f86a
EAS0314 <111325608+EAS0314@users.noreply.github.com>
2023-03-27 19:52:30 -0500

    Changes made to styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { TheTeamComponent } from './the-team/the-team.component';
+import { AbovethefoldComponent } from './abovethefold/abovethefold.component';
+
 const routes: Routes =[
-  { path: '', component: HomeComponent },
-  { path: 'AboutUs', component: AboutUsComponent},
-  { path: 'TheTeam', component: TheTeamComponent},
+  {path: '', component: AbovethefoldComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent},
+  { path: 'the-team', component: TheTeamComponent},
 ];
 
 @NgModule({
