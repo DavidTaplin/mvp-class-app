8dfce30e31aa8459e0cb4ba4955a0047f6b386cb
jonbeal93 <jonbeal93@gmail.com>
2023-04-05 21:59:56 -0500

    Added Token, added logout func
    Token uses local storage for auto login
    Log Out func logs out user and the button is only visible on the navbar if user is logged in

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -14,7 +15,7 @@ export class LoginComponent implements OnInit {
     email: new FormControl(''),
     password: new FormControl(''),
   });
-  constructor(private authservice: AuthService, private userService:UserService) {}
+  constructor(private authservice: AuthService, private userService:UserService, private route:Router) {}
 
   ngOnInit(): void {}
 
@@ -22,7 +23,10 @@ export class LoginComponent implements OnInit {
     const loginuser = this.loginForm.value;
     this.authservice.login(loginuser).subscribe((res: any) => {
       if(res.success){
-        this.userService.setCurrentUser(res.payload.user);
+        this.userService.setCurrentUser(res.payload.user)
+        this.route.navigate(['/home'])
+        this.authservice.setToken(res.payload.token)
+        console.log(res)
       }
     });
   }
--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/auth/user.service';
 import { User } from '../models/user.model';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,15 +11,14 @@ import { User } from '../models/user.model';
 export class NavComponent implements OnInit {
   currentUser: User = null;
 
-  constructor(private userService:UserService) { }
+  constructor(private userService:UserService, private authService:AuthService) { }
 
   ngOnInit(): void {
     this.userService.currentUserSubject.subscribe((user:User)=>{
       this.currentUser = user;
   })
-
-
-
 }
-
+  onLogout(){
+    this.authService.logout();
+  }
 }
