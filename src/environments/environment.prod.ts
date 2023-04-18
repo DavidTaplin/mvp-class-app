78f2f42754739f6bbb9dcb567ccffcc966f8fd39
Joe Letner <jrletner@gmail.com>
2023-04-17 21:40:38 -0500

    updated environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  RapidAPI: {
+    ApiUrl: 'https://moviesminidatabase.p.rapidapi.com/movie/imdb_id/byTitle/',
+    rapidAPI_Key:
+      '&rapidapi-key=1c98263760msh3afed6ceb16c7e8p1b1dedjsn9feb4f2c3e63',
+  },
 };
